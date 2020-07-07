a63f5d1b9e943ebe28dbf61ac95140950aa87896
MatteoFusconi <66904987+MatteoFusconi@users.noreply.github.com>
2020-07-07 16:59:45 +0200

    Update structures.hpp

--- structures.hpp
+++ structures.hpp
@@ -1,40 +1,39 @@
 #pragma once
 #include<iostream>
 
-    enum class Sir :char {
-        s, i, r, q, q_edge
-    };
-    enum class Mode : int {
-        Still, Move, move_plus, Quarantene_1, Quarantene_2, Quarantene_1_and_2
-    };
-    // struttura che definisce i paramtri della quarantena
-    struct Quarantene_parameters {
-        int first_day = 0;
-        int last_day = 0;
-        int len_line = 0;
-    };
-    //struttura che definisce i parametri di ogni cella del vettore grid della classe board.
-    struct Cell {
-        Sir state;
-        double inf_prob = 0; //Probabilità di infettarsi
-        int clock;
-        inline Cell() {
-            state = Sir::s;
-            inf_prob = 0;
-            clock = 0;
-
-        }
-    };
-    struct Counter {
-        int num_s=0;
-        int num_i=0;
-        int num_r=0;
-    };    
-    inline Sir& operator++(Sir& hs) {
-        switch (hs) {
-        case Sir::s: hs = Sir::i; return hs; //++s = i
-        case Sir::i: hs = Sir::r; return hs; //++i = r
-        case Sir::q: hs = Sir::r; return hs; //++q = r
-        default: return hs;
-        }
-    };
+enum class Sir :char {
+    s, i, r, q, q_edge
+};
+enum class Mode : int {
+    Still, Move, Move_Plus, Quarantine_1, Quarantine_2, Quarantine_1_and_2
+};
+// struttura che definisce i paramtri della quarantena
+struct Quarantine_parameters {
+    int first_day = 0;
+    int last_day = 0;
+    int len_line = 0;
+};
+//struttura che definisce i parametri di ogni cella del vettore grid della classe board.
+struct Cell {
+    Sir state;
+    double inf_prob = 0; //Probabilità di infettarsi
+    int clock;
+    inline Cell() {
+        state = Sir::s;
+        inf_prob = 0;
+        clock = 0;
+    }
+};
+struct Counter {
+    int num_s = 0;
+    int num_i = 0;
+    int num_r = 0;
+};
+inline Sir& operator++(Sir& hs) {
+    switch (hs) {
+    case Sir::s: hs = Sir::i; return hs; //++s = i
+    case Sir::i: hs = Sir::r; return hs; //++i = r
+    case Sir::q: hs = Sir::r; return hs; //++q = r
+    default: return hs;
+    }
+};
